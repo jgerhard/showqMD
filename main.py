dbbad0013c764733054f15fd1260c2df109b1d73
Jochen Gerhard <jochen.gerhard@compeng.uni-frankfurt.de>
2013-09-16 17:10:57 +0200

    saving hadrons and partons

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import physics
 import initialize
 import time
 from numpy import savetxt, concatenate, sqrt
-from create_hadrons import create_meson, create_baryon
-
+from find_neighbours import create_candidates
+    
 #max number of particles
-maxnum = 6000 
+maxnum = 6000
 #time step for integration
 dt = 1e-3                      # 1e-4 leaves mass of meson at hadronization constant for all timesteps
 #number of timesteps
-run_time = 1                  # run time in fm/c
-save_time = 0.01               # timesteps to be saved in fm/c
+run_time = 10                  # run time in fm/c
+save_time = 0.05               # timesteps to be saved in fm/c
 
 class Simulation():
     def __init__(self, maxnum=maxnum, dt=dt):
@@ -51,24 +51,23 @@ class Simulation():
         """ Experimental isochronal hadronization on host """
         
         (pos, mommass, col, force) = self.cle.pullData()
-        partons = concatenate((pos, mommass, force, col),1)
-        if len(partons) == 3:
-            hadron = create_baryon(*partons)
-        else:
-            hadron = create_meson(*partons)
-        return hadron
+        partons = list(concatenate( (pos, mommass, force, col) ,1).tolist())
+        baryons, mesons = create_candidates(partons)
+        return baryons, mesons
 
 
 def run():
     print("Simulating %f fm/c"%run_time)
     MyRun = Simulation()
-    while (MyRun.totaltime < run_time):
-        MyRun.run(MyRun.totaltime + save_time)
-        MyRun.save()
-
+    MyRun.run(run_time)
+    MyRun.save()
+    baryons, mesons = MyRun.hadronize()
+    savetxt("hadrons.csv", baryons+mesons, delimiter=",")
 
 if __name__ == "__main__":
     run()
+    
+    
 
 
 
